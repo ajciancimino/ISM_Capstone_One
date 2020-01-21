1b09098bab5c17b872682bb107461d2b83e72540
Ayrton Ciancimino <ajciancimino@mail.usf.edu>
2020-01-21 13:52:51 -0500

    Initial commit

---
+++ ISM_Capstone_One/ISM_Capstone_One/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM_Capstone_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
